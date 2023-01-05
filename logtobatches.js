4508907f42b4120b73e9507aed8f70ea0b8befb9
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2023-01-05 16:38:58 +0100

    More refined filters.

--- logtobatches.js
+++ logtobatches.js
@@ -5,21 +5,83 @@ const fs = require("fs"),
 const visitDurationMillis = 300 * 1000; // 5 min of no traffic signifies a new visit.
 const ignored_paths = ["/v1/queue-pinservice/pins", "/server_status"]; // Paths we're not testing for.
 const ignored_status = [301, 400, 404]; // This won't actully hit our backend.
-const ignored_remotes = ["130.245.145.0", "148.71.182.0"]; // Skip known bots.
+
+const ignored_agents = ["python-requests", "k6", "Googlebot", "Java"]; // Get rid of bots.
+
+// Skip known bots.
+// gzcat  *.gz  | grep '/v1/search?q=.*page=60' | cut -d ' ' -f 1 | sort | uniq | jq --raw-input --slurp 'split("\n")'
+const ignored_remotes = [
+  "114.218.30.0",
+  "115.87.153.0",
+  "116.87.138.0",
+  "124.120.206.0",
+  "129.49.100.0",
+  "130.245.145.0",
+  "141.85.144.0",
+  "148.71.182.0",
+  "161.230.143.0",
+  "184.103.221.0",
+  "185.220.100.0",
+  "20.48.28.0",
+  "2001:ac8::",
+  "2601:1c1::",
+  "2a0f:93c1::",
+  "34.252.169.0",
+  "46.10.12.0",
+  "46.204.100.0",
+  "46.32.86.0",
+  "49.216.183.0",
+  "58.11.156.0",
+  "58.11.157.0",
+  "58.11.188.0",
+  "65.109.59.0",
+  "76.115.107.0",
+  "77.101.140.0",
+  "79.231.8.0",
+  "80.195.236.0",
+  "80.75.118.0",
+  "86.49.124.0",
+  "89.74.70.0",
+  "91.159.232.0",
+  "91.181.168.0",
+];
+
 const max_visit_steps = 256; // Reduce oversized batches; reduces crawlers.
 const dataFile = "visits.json";
+
+function shouldIgnore(parsed) {
+  if (ignored_remotes.includes(parsed.remote_addr)) {
+    // console.log(`Ignoring remote ${parsed.remote_addr}`);
+    return true;
+  }
+
+  if (ignored_status.includes(parsed.status)) {
+    // console.log(`Ignoring status ${parsed.status}`);
+    return true;
+  }
+
+  if (
+    ignored_agents.some(
+      (agent) =>
+        parsed.http_user_agent && parsed.http_user_agent.includes(agent)
+    )
+  ) {
+    // console.log(`Ignoring agent ${parsed.http_user_agent}`);
+    return true;
+  }
+
+  if (ignored_paths.some((path) => parsed.path.includes(path))) {
+    // console.log(`Ignoring path ${parsed.path}`);
+    return true;
+  }
+
+  return false;
+}
 
 async function parseLineToRemotes(line, remotes) {
   const parsed = parse(line);
 
-  // Filter out ignored stuff.
-  if (
-    ignored_paths.includes(parsed.path) ||
-    ignored_status.includes(parsed.status) ||
-    ignored_remotes.includes(parsed.remote_addr)
-  ) {
-    return;
-  }
+  if (shouldIgnore(parsed)) return;
 
   const rKey = parsed.remote_addr;
 
@@ -40,8 +102,8 @@ async function parseLineToRemotes(line, remotes) {
       console.log(`New visit from existing remote ${parsed.remote_addr}`);
       visits.push([parsed]);
     } else {
-      // Add request to last visit
-      visits[visits.length - 1].push(parsed);
+      // Add request to latest visit
+      latest_visit.push(parsed);
     }
   }
 }
